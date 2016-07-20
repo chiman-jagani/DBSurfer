b4bd7091d4ac47072478a785eaced928a86b9380
chiman-jagani <chiman_jagani@hotmail.com>
2016-07-20 16:28:10 +0530

    DBSurfer 64bit and 32bit

---
+++ 32/CopyDLL.bat
@@ -0,0 +1,22 @@
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\libjcc.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\pbacc126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\pbdpl126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\pbdwe126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\pbjvm126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\pbshr126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\pbtra126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\pbtrs126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\pbvm126.dll" .
+
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\pbuis126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\pbora126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\pbo10126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\pbo90126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\pbodb126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\pbsnc126.dll" .
+
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\libjutils.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\nlwnsck.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\libjtml.dll" .
+@pause
+
---
+++ 64/CopyDLL.bat
@@ -0,0 +1,18 @@
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\x64\libjcc.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\x64\pbacc126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\x64\pbdpl126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\x64\pbdwe126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\x64\pbjvm126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\x64\pbshr126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\x64\pbtra126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\x64\pbtrs126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\x64\pbvm126.dll" .
+
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\x64\pbuis126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\x64\pbora126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\x64\pbo10126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\x64\pbo90126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\x64\pbodb126.dll" .
+copy "C:\Program Files (x86)\Sybase\Shared\PowerBuilder\x64\pbsnc126.dll" .
+@pause
+
